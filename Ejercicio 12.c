98e10f81f69ea96c9a730037054173f6eee25917
BRAM-25 <abram.zuni2@gmail.com>
2025-05-16 12:12:30 -0600

    Create Ejercicio 12.c

---
+++ Ejercicio 12.c
@@ -0,0 +1,24 @@
+//Abraham Zuñiga
+//Ejercicio12
+//Producto de los N primeros números naturales.
+
+#include <stdio.h>
+
+int main() 
+{
+
+    int n;
+    long long producto = 1;
+
+    printf("Ingrese el valor de N (número de números naturales a multiplicar): ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) 
+    {
+        producto *= i;
+    }
+
+    printf("El producto de los primeros %d números naturales es: %lld\n", n, producto);
+
+    return 0;
+}
